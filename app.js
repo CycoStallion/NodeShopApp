2945d23117754fec8db6dde6c27ceed534b6c732
CycoStallion <30428564+CycoStallion@users.noreply.github.com>
2020-07-03 17:17:18 +0530

    Added middleware and sent response from the last one to the client

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ const express = require('express');
 
 const app = express();
 
+//Custom Middleware
+app.use((req, res, next) => {
+    console.log("In the middleware");
+    next(); //Allows the request to continue to the next middleware
+});
+
+app.use((req, res, next) => {
+    console.log("In another middleware");
+    res.send('<h1>Hello from Express JS</h1>');
+});
+
 //Create an http server to serve requests
 const server = http.createServer(app);
 
