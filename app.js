bb56bb88ffd4575460a6c02866efd89cbf1d676c
CycoStallion <30428564+CycoStallion@users.noreply.github.com>
2020-07-03 20:20:46 +0530

    Added body parser to read incoming data from a form post

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const { reset } = require('nodemon');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+//BodyParser
+app.use(bodyParser.urlencoded({extended: true}))
 
 //Custom Middleware
 app.use((req, res, next) => {
@@ -10,9 +15,14 @@ app.use((req, res, next) => {
 });
 
 //Route for /product-page
-app.use('/product-page', (req, res, next) => {
+app.use('/product', (req, res, next) => {
+    console.log(req.body); //Will show undefined w/o body parser
     res.send('<h1>This is the product page</h1>');
-})
+});
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Post</button></form>');
+});
 
 app.use((req, res, next) => {
     console.log("In another middleware");
