7c54bfa011ce34da7e9dd9525dd738eefb45b90f
CycoStallion <30428564+CycoStallion@users.noreply.github.com>
2020-07-03 23:37:26 +0530

    Added 404 html template and serve it for unknown paths

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -21,7 +22,9 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>');
+    // res.status(404).send('<h1>Page not found</h1>');
+    res.status(404)
+        .sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 
