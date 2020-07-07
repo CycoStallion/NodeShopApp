0d7be7e6c15b151a83e15b9697448ea6ab16e682
CycoStallion <30428564+CycoStallion@users.noreply.github.com>
2020-07-07 23:14:38 +0530

    Added partials for ejs files

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
     // res.status(404).send('<h1>Page not found</h1>');
     res.status(404)
         //.sendFile(path.join(__dirname, 'views', '404.html'));
-        .render('404', {pageTitle: 'Page Not Found'});
+        .render('404', {activePath: '', pageTitle: 'Page Not Found'});
 });
 
 
