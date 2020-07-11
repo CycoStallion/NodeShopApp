1ec1a5ae3638bbba584db32d80c30cf1de6369fd
CycoStallion <30428564+CycoStallion@users.noreply.github.com>
2020-07-11 20:18:16 +0530

    Added functionality to read product data from file Fixes #27

--- models/product.js
+++ models/product.js
@@ -1,4 +1,20 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, '../', 'data', 'products.json');
+
+const getAllProductsFromFile = (callback) => {
+    fs.readFile(filePath, (err, fileData) => {
+        if(err){
+            return callback([]);
+        }
+        try {
+            callback(JSON.parse(fileData))
+        } catch (error) {
+            callback([]);
+        }
+    })
+}
 
 class Product{
     constructor(productTitle){
@@ -6,11 +22,19 @@ class Product{
     }
 
     save(){
-        products.push(this.title);
+        getAllProductsFromFile((products) => {
+            //Add new product 
+            products.push(this.title);
+
+            //Write back to file with new data
+            fs.writeFile(filePath, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
     }
 
-    static fetchAll(){
-        return products;
+    static fetchAll(callback){
+        getAllProductsFromFile(callback);
     }
 }
 
