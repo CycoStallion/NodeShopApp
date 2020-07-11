cc924c0b34119335223f261a0bd98a4b7b51f47c
CycoStallion <30428564+CycoStallion@users.noreply.github.com>
2020-07-11 22:37:49 +0530

    Segregation of functionalities into differentfolders and adde views for the same

--- controllers/productController.js
+++ controllers/productController.js
@@ -6,7 +6,7 @@ getProducts = (req, res, next) => {
     Product.fetchAll((products) => {
         console.log('Stored products on variable: ', products);
         // res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
-        res.render('shop', {products, pageTitle:'Shop', activePath: "/"}); //Render the shop view. Its path and format is already mentioned in the app.js configuration
+        res.render('shop/product-list', {products, pageTitle:'Shop', activePath: "/"}); //Render the shop view. Its path and format is already mentioned in the app.js configuration
     });
     
 };
@@ -14,7 +14,7 @@ getProducts = (req, res, next) => {
 getAddProducts = (req, res, next) => {
     // res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Post</button></form>');
     // res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
-    res.render('add-product', {pageTitle:'Add Product', activePath: "/admin/add-product"});
+    res.render('admin/add-product', {pageTitle:'Add Product', activePath: "/admin/add-product"});
 };
 
 postAddProducts = (req, res, next) => {
