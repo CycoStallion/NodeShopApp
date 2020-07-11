6c64067bbc6ee9c6cf2bbb936b519971f0e2a731
CycoStallion <30428564+CycoStallion@users.noreply.github.com>
2020-07-11 16:00:38 +0530

    Added 404 controller and imported it in app.js

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+const pageNotFound = require('./controllers/404Controller');
+
 //BodyParser
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -32,12 +34,7 @@ app.use((req, res, next) => {
     next(); //Allows the request to continue to the next middleware
 });
 
-app.use((req, res, next) => {
-    // res.status(404).send('<h1>Page not found</h1>');
-    res.status(404)
-        //.sendFile(path.join(__dirname, 'views', '404.html'));
-        .render('404', {activePath: '', pageTitle: 'Page Not Found'});
-});
+app.use(pageNotFound);
 
 
 
